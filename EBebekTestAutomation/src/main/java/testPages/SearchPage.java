0e31f502ea294c813e3057d66b03b8b5fa4da605
İbrahim <68855464+cuhadar8@users.noreply.github.com>
2022-11-28 13:29:01 +0300

    Update SearchPage.java

--- EBebekTestAutomation/src/main/java/testPages/SearchPage.java
+++ EBebekTestAutomation/src/main/java/testPages/SearchPage.java
@@ -3,18 +3,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+
 
 public class SearchPage {
 
 	private WebDriver driver;
-	private Actions actions;
 	
 
 	public SearchPage(WebDriver driver) {
 		super();
 		this.driver = driver;
-		actions = new Actions(driver);
 	}
 
 	
