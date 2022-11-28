c54b32f76fb0c36e7299f95469883133287a1929
İbrahim <68855464+cuhadar8@users.noreply.github.com>
2022-11-28 13:24:15 +0300

    Update HomePage.java

--- EBebekTestAutomation/src/main/java/testPages/HomePage.java
+++ EBebekTestAutomation/src/main/java/testPages/HomePage.java
@@ -3,19 +3,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class HomePage {
 	
 	private WebDriver driver;
 	private By searchBoxPath = By.id("txtSearchBox");   //Sitedeki arama çubuğunun konumunu ID ile belirledim.
-	public Actions actions;
 	
 	
 	public HomePage(WebDriver driver) {
 		super();
 		this.driver = driver;
-		actions = new Actions(driver);
 	}
 
 
