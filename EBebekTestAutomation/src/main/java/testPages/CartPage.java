52e7ebf051d1923858bdd58fd6ca1a0e6ebbe9a5
İbrahim <68855464+cuhadar8@users.noreply.github.com>
2022-11-28 13:21:58 +0300

    Update CartPage.java

--- EBebekTestAutomation/src/main/java/testPages/CartPage.java
+++ EBebekTestAutomation/src/main/java/testPages/CartPage.java
@@ -2,16 +2,14 @@ package testPages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class CartPage {
 	
 	private WebDriver driver;
-	public Actions actions;
+	
 	
 	public CartPage(WebDriver driver) {
 		this.driver = driver;
-		actions = new Actions(driver);
 	}
 	
 	public void completeShopping() throws InterruptedException {  //alışverişi tamamla butonuna tıkladım
